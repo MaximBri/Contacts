6f591ed3b5ef04d262ea78a15442a968314dc631
Maxim <MaximK38136@yandex.ru>
2025-06-29 17:42:59 +0400

    feat: base project

---
+++ src/components/MainMenu.tsx
@@ -0,0 +1,22 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export const MainMenu = () => {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Container>
+        <Navbar.Brand href='/'>
+          <h1>Книга контактов</h1>
+        </Navbar.Brand>
+        <Nav className='me-auto'>
+          <Nav.Link as={Link} to='/groups'>
+            Группы
+          </Nav.Link>
+          <Nav.Link as={Link} to='/favorit'>
+            Избранное
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  )
+}
