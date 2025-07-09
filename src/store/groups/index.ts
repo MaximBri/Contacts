13fc28db52b816fecb7dcbc96334e96ece5a80fe
Maxim <MaximK38136@yandex.ru>
2025-07-09 11:44:13 +0400

    chore: deleted redux stores

--- src/store/groups/index.ts
+++
@@ -1,6 +0,0 @@
-import { groupsApi } from './slice'
-
-export const groupsReducer = groupsApi.reducer
-export const groupsReducerPath = groupsApi.reducerPath
-
-export const { useGetGroupsQuery } = groupsApi
